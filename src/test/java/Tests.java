e6cc07f4607e300832bc141903bd88c802cf9b7b
Mateusz Krasny <mkrasny@opidomain.local>
2019-07-25 14:26:15 +0200

    onet

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -39,7 +39,7 @@ public class Tests {
 
 
         log.info("1. Przejscie na strone logowania");
-        open("https://programs.nawa.gov.pl/login");
+        open("https://onet.pl/");
         //getWebDriver().manage().window().maximize();
 
         new HomePage().search("Tarnobrzeg");
